1750b6057bf8513dbe67f344648ed8ee47cfcb35
Fsher07 <furkansalimm@gmail.com>
2022-06-23 23:20:55 +0300

    Create book,person and rental classes

--- person.rb
+++ person.rb
@@ -7,11 +7,12 @@ class Person < Nameable
     @name = name
     @age = age
     @parent_permission = parent_permission
+    rentals = []
   end
 
-  attr_reader :id
+  attr_reader :id, :rentals
 
-  attr_accessor :name, :age
+  attr_accessor :name, :age, :parent_permission
 
   def of_age?
     @age >= 18

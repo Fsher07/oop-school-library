b23a2c9f17406403d6cb19368e7db1965a82e435
Fsher07 <furkansalimm@gmail.com>
2022-06-23 23:20:27 +0300

    Create classrom and student classes

--- student.rb
+++ student.rb
@@ -1,9 +1,17 @@
 require './person'
+require './classroom'
 
 class Student < Person
-  def initialize(classroom, age, name = 'unknown', parent_permission: true)
-    super(name, age, parent_permission)
+  attr_accessor :classroom, :name, :age
+
+  def initialize(age, name = 'unknown')
+    super(age, name)
     @classroom = classroom
+  end
+
+  def classroom=(classroom)
+    @classroom = classroom
+    classroom.students.push(self) unless classroom.students.include?(self)
   end
 
   def play_hooky
